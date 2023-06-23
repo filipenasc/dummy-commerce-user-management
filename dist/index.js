ef912fe6849a5717894fb2256c6962ba725b7fe5
Filipe Nascimento <filipemateusn@gmail.com>
2023-06-23 17:58:11 -0300

    Update start command

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,23 @@
-import http from 'http';
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const routes_1 = require("./routes");
+const app = (0, express_1.default)();
+const port = parseInt(process.env.PORT) || 8080;
+app.use((0, cors_1.default)({
+    origin: '*'
+}));
+mongoose_1.default.connect(process.env.MONGODB_CONNECTION);
+// Middleware to parse JSON body
+app.use(express_1.default.json());
+app.use(routes_1.router);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
 //# sourceMappingURL=index.js.map
